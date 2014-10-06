e1d963e9e543057b2b7ac095e9ce89ea84eb813c
Martin Helmich <m.helmich@mittwald.de>
2014-10-06 21:38:39 +0200

    Add FrontendUserGroupRepository.

---
+++ Classes/Mw/T3Compat/Domain/Repository/FrontendUserGroupRepository.php
@@ -0,0 +1,20 @@
+<?php
+namespace Mw\T3Compat\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Mw.T3Compat".           *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class FrontendUserGroupRepository extends Repository
+{
+
+
+
+}
